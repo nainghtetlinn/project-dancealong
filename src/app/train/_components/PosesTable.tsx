09e286a613369969e7a6d8fc413adcb79405eb21
nainghtetlinn <naing.work.it@gmail.com>
2025-04-18 18:20:33 +0630

    create poses table

---
+++ src/app/train/_components/PosesTable.tsx
@@ -0,0 +1,51 @@
+import { Button } from '@/components/ui/button'
+import { DataTable } from '@/components/ui/data-table'
+import { useTrain } from '@/provider/train-provider'
+import { TPose } from '@/types/pose'
+import { ColumnDef } from '@tanstack/react-table'
+import { Trash, Edit, Video } from 'lucide-react'
+
+const columns: ColumnDef<TPose>[] = [
+  { accessorKey: 'label', header: 'Label' },
+  { accessorKey: 'numOfPosesCaptured', header: '' },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      return (
+        <div className='flex justify-end gap-2'>
+          <Button
+            className='bg-green-500'
+            size='icon'
+          >
+            <Video />
+          </Button>
+          <Button
+            variant='secondary'
+            size='icon'
+          >
+            <Edit />
+          </Button>
+          <Button
+            variant='destructive'
+            size='icon'
+          >
+            <Trash />
+          </Button>
+        </div>
+      )
+    },
+  },
+]
+
+const PosesTable = () => {
+  const { poses } = useTrain()
+
+  return (
+    <DataTable
+      columns={columns}
+      data={poses}
+    />
+  )
+}
+
+export default PosesTable
---
+++ src/types/pose.ts
@@ -0,0 +1,4 @@
+export type TPose = {
+  label: string
+  numOfPosesCaptured: number
+}

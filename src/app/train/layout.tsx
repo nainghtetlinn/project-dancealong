bbbfe2d6fb534b84168308cde0fa1829acd78979
nainghtetlinn <naing.work.it@gmail.com>
2025-04-21 19:21:05 +0630

    extract layout for code readability

--- src/app/train/layout.tsx
+++ src/app/train/layout.tsx
@@ -1,7 +1,12 @@
-import StoreProvider from '@/provider/store-provider'
+import Navbar from './_components/Navbar'
 
 const TrainLayout = ({ children }: { children: React.ReactNode }) => {
-  return <StoreProvider>{children}</StoreProvider>
+  return (
+    <div className='flex flex-col h-screen'>
+      <Navbar />
+      <main className='flex-1'>{children}</main>
+    </div>
+  )
 }
 
 export default TrainLayout
--- src/app/train/page.tsx
+++ src/app/train/page.tsx
@@ -1,34 +1,29 @@
 'use client'
 
 import Controls from './_components/Controls'
-import Navbar from './_components/Navbar'
 import Studio from './_components/Studio'
 
 import { AudioProvider } from '@/provider/audio-provider'
-import { TrainProvider } from '@/provider/train-provider'
+import StoreProvider from '@/provider/store-provider'
 import { WebcamProvider } from '@/provider/webcam-provider'
 
 const Train = () => {
   return (
-    <TrainProvider>
+    <StoreProvider>
       <AudioProvider>
         <WebcamProvider>
-          <section className='flex flex-col h-screen'>
-            <Navbar />
-
-            <div className='flex-1 flex'>
-              <div className='p-2'>
-                <Studio />
-              </div>
-
-              <div className='flex-1 p-2 overflow-hidden'>
-                <Controls />
-              </div>
+          <div className='flex'>
+            <div className='p-2'>
+              <Studio />
             </div>
-          </section>
+
+            <div className='flex-1 p-2 overflow-hidden'>
+              <Controls />
+            </div>
+          </div>
         </WebcamProvider>
       </AudioProvider>
-    </TrainProvider>
+    </StoreProvider>
   )
 }
 

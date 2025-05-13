b085aba94c62b40466cd37da83e8e1dbbfbb739c
nainghtetlinn <naing.work.it@gmail.com>
2025-05-13 16:31:40 +0630

    feat: enhance AudioUpload component with form handling and improved UI for audio uploads

--- src/app/admin/[projectId]/_components/AudioUpload.tsx
+++ src/app/admin/[projectId]/_components/AudioUpload.tsx
@@ -1,60 +1,180 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 import { Loader2, Upload } from 'lucide-react'
 import Dropzone from 'react-dropzone'
+import MiniWaveForm from '../../_components/MiniWaveForm'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { useState } from 'react'
+import { toast } from 'sonner'
 
 import { getAudioDuration } from '@/lib/audio'
 import { cn } from '@/lib/utils'
-import { useState } from 'react'
-import { uploadAudio } from '../../action'
+import { uploadAudio } from '@/server-actions/audio'
+import { uploadAudioSchema } from '@/validators/audio-validator'
 import { useAudio } from '../_lib/audioContext'
 import { useProjectDetails } from '../_lib/projectContext'
+
+type TForm = z.infer<typeof uploadAudioSchema>
 
 const AudioUpload = () => {
   const { projectId } = useProjectDetails()
   const { upload } = useAudio()
 
+  const form = useForm<TForm>({
+    resolver: zodResolver(uploadAudioSchema),
+    defaultValues: {
+      title: '',
+      artist: '',
+      duration: 0,
+    },
+  })
+
+  const [file, setFile] = useState<File | null>(null)
+  const [fileLocalUrl, setFileLocalUrl] = useState('')
   const [loading, setLoading] = useState(false)
 
   const handleDrop = async (acceptedFiles: File[]) => {
     const audio = acceptedFiles[0]
-    setLoading(true)
     const duration = await getAudioDuration(audio)
-    await uploadAudio(audio, duration, projectId)
-    setLoading(false)
-    upload(audio)
+    form.setValue('duration', duration)
+    setFile(audio)
+    setFileLocalUrl(URL.createObjectURL(audio))
   }
 
-  if (loading) return <Loader2 className='animate-spin' />
+  const onSubmit = async (values: TForm) => {
+    setLoading(true)
+    const result = await uploadAudio(values, file!, projectId)
+
+    if (!result.success) toast.error(result.message)
+    else {
+      toast.success('Successfully uploaded.')
+      upload(file!) // upload to audio context
+    }
+
+    setLoading(false)
+  }
 
   return (
-    <Dropzone onDrop={handleDrop}>
-      {({ getRootProps, getInputProps, isDragActive }) => (
-        <div {...getRootProps()}>
-          <input
-            {...getInputProps()}
-            accept='audio/*'
-          />
-          <div
-            className={cn(
-              'p-2 w-[350px] aspect-square flex items-center justify-center bg-background cursor-pointer rounded-2xl border-2 border-dashed border-primary',
-              isDragActive && 'bg-primary/30'
-            )}
-          >
-            <div>
-              <Upload
-                size={40}
-                className='mx-auto'
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <Card className='w-[350px]'>
+          <CardHeader>
+            <CardTitle>Upload a song</CardTitle>
+            <CardDescription>Just give title and artist name.</CardDescription>
+          </CardHeader>
+          <CardContent className='space-y-4'>
+            <FormField
+              control={form.control}
+              name='title'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='artist'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Artist name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {file === null ? (
+              <Dropzone onDrop={handleDrop}>
+                {({ getRootProps, getInputProps, isDragActive }) => (
+                  <div {...getRootProps()}>
+                    <input
+                      {...getInputProps()}
+                      accept='audio/*'
+                    />
+                    <div
+                      className={cn(
+                        'w-full h-[150px] flex items-center justify-center bg-background cursor-pointer rounded-2xl border-2 border-dashed border-primary',
+                        isDragActive && 'bg-primary/30'
+                      )}
+                    >
+                      <div>
+                        <Upload
+                          size={40}
+                          className='mx-auto'
+                        />
+                        <p className='text-center font-bold'>
+                          Drag & drop to upload song
+                        </p>
+                        <p className='text-center text-sm text-primary'>
+                          or browse
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </Dropzone>
+            ) : (
+              <FormField
+                control={form.control}
+                name='duration'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Duration</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        disabled
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
               />
-              <p className='text-center font-bold'>
-                Drag & drop to upload song
-              </p>
-              <p className='text-center text-sm text-primary'>or browse</p>
-            </div>
-          </div>
-        </div>
-      )}
-    </Dropzone>
+            )}
+            {fileLocalUrl && (
+              <div className='mt-8'>
+                <MiniWaveForm url={fileLocalUrl} />
+              </div>
+            )}
+          </CardContent>
+          <CardFooter className='justify-end'>
+            <Button
+              type='submit'
+              disabled={loading}
+            >
+              Upload {loading && <Loader2 className='animate-spin' />}
+            </Button>
+          </CardFooter>
+        </Card>
+      </form>
+    </Form>
   )
 }
 

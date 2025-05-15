773a9b0efd7c1440bad5cbb0236ec398b85e525a
nainghtetlinn <naing.work.it@gmail.com>
2025-05-16 00:06:15 +0630

    feat: add useWebcam hook for webcam access and control

---
+++ src/hooks/useWebcam.ts
@@ -0,0 +1,44 @@
+'use client'
+
+import { useRef, useState } from 'react'
+
+const useWebcam = (width: number, height: number) => {
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const streamRef = useRef<MediaStream>(null)
+
+  const [isEnable, setIsEnable] = useState(false)
+  const [isError, setIsError] = useState(false)
+
+  const enable = async (): Promise<void> => {
+    setIsEnable(false)
+    setIsError(false)
+    try {
+      streamRef.current = await navigator.mediaDevices.getUserMedia({
+        video: {
+          width: { ideal: width },
+          height: { ideal: height },
+        },
+      })
+      if (videoRef.current) videoRef.current.srcObject = streamRef.current
+      setIsEnable(true)
+      setIsError(false)
+    } catch (error) {
+      setIsEnable(false)
+      setIsError(true)
+    }
+  }
+
+  const disable = () => {
+    if (streamRef.current) {
+      streamRef.current.getTracks().forEach(track => track.stop())
+      streamRef.current = null
+      if (videoRef.current) videoRef.current.srcObject = null
+      setIsEnable(false)
+      setIsError(false)
+    }
+  }
+
+  return { videoRef, isEnable, isError, enable, disable }
+}
+
+export default useWebcam

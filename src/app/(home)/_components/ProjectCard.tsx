32164a695e684427d0f057139fd81158a3b8328b
nainghtetlinn <naing.work.it@gmail.com>
2025-05-16 13:00:42 +0630

    feat: implement ProjectCard component and integrate it into Home page

---
+++ src/app/(home)/_components/ProjectCard.tsx
@@ -0,0 +1,47 @@
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import MiniWaveForm from '@/app/admin/_components/MiniWaveForm'
+
+import { type TProject } from '@/types'
+
+import { formatTime } from '@/lib/utils'
+import Link from 'next/link'
+
+export default function ProjectCard({ project }: { project: TProject }) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{project.project_name}</CardTitle>
+
+        <CardDescription>
+          {project.songs
+            ? `${project.songs.title} | ${formatTime(project.songs.duration)}`
+            : 'No song uploaded yet.'}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        {project.songs ? (
+          <MiniWaveForm url={project.songs.audio_url} />
+        ) : (
+          <div className='h-[60px]'></div>
+        )}
+      </CardContent>
+      <CardFooter className='justify-end gap-2'>
+        <Button
+          size='sm'
+          variant='secondary'
+          asChild
+        >
+          <Link href={`/play/${project.project_name}`}>Play</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  )
+}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <div>Hello</div>
+import ProjectCard from './_components/ProjectCard'
+
+import { redirect } from 'next/navigation'
+import { createClient } from '@/utils/supabase/server'
+
+export default async function Home() {
+  const supabase = await createClient()
+
+  const { error, data } = await supabase
+    .from('projects')
+    .select('*, songs(*)')
+    .not('song_id', 'is', null)
+
+  if (error) redirect('/error')
+
+  return (
+    <main>
+      <h5 className='text-center text-2xl font-bold p-2'>Dancealong</h5>
+      <div className='p-2 mx-auto container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
+        {data.map(project => (
+          <ProjectCard
+            key={project.id}
+            project={project}
+          />
+        ))}
+      </div>
+    </main>
+  )
 }

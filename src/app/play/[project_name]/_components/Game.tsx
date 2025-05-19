5753129bad30575268ba944f1ac54883aaa9fb0b
nainghtetlinn <naing.work.it@gmail.com>
2025-05-19 23:07:44 +0630

    feat: add choreography prop to Game component and improve layout structure

--- src/app/play/[project_name]/_components/Game.tsx
+++ src/app/play/[project_name]/_components/Game.tsx
@@ -4,7 +4,7 @@ import { Loader2 } from 'lucide-react'
 import AudioTimeline from './AudioTimeline'
 import Room from './Room'
 
-import { type TSong, TProject } from '@/types'
+import { TParsedChoreography, TProject, TSong } from '@/types'
 
 import { formatTime } from '@/lib/utils'
 import { useAudio } from '../_lib/audioContext'
@@ -12,35 +12,39 @@ import { useAudio } from '../_lib/audioContext'
 export default function Game({
   project,
   song,
+  choreography,
 }: {
   project: TProject
   song: TSong
+  choreography: TParsedChoreography[]
 }) {
   const { loading } = useAudio()
 
   if (loading)
     return (
-      <div className='h-full flex items-center justify-center'>
+      <div className='h-screen flex items-center justify-center'>
         <Loader2 className='animate-spin' />
       </div>
     )
 
   return (
     <>
-      <div className='absolute top-0 left-0 right-0 flex justify-between items-center'>
-        <div className='p-2'>
-          <h6 className='font-bold text-sm'>{project.project_name}</h6>
-          <h4 className='font-bold text-2xl'>{song.title}</h4>
-          <p>
-            {song.artist} | {formatTime(song.duration)}
-          </p>
-        </div>
-        <div>
-          <AudioTimeline />
-        </div>
-      </div>
+      <div className='h-screen overflow-hidden relative'>
+        <section className='absolute top-0 left-0 right-0 z-10 flex justify-between items-center'>
+          <div className='p-2'>
+            <h6 className='font-bold text-sm'>{project.project_name}</h6>
+            <h4 className='font-bold text-2xl'>{song.title}</h4>
+            <p>
+              {song.artist} | {formatTime(song.duration)}
+            </p>
+          </div>
+          <div className='p-2 w-[450px]'>
+            <AudioTimeline />
+          </div>
+        </section>
 
-      <Room />
+        <Room choreography={choreography} />
+      </div>
     </>
   )
 }

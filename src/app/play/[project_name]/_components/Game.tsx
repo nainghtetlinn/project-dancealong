750c0283df7bdcee6fb93e5f0ba2edba8e83e341
nainghtetlinn <naing.work.it@gmail.com>
2025-05-20 11:22:24 +0630

    fix: improve layout structure in Game component for better readability

--- src/app/play/[project_name]/_components/Game.tsx
+++ src/app/play/[project_name]/_components/Game.tsx
@@ -29,16 +29,15 @@ export default function Game({
 
   return (
     <>
-      <div className='h-screen overflow-hidden relative'>
-        <section className='absolute top-0 left-0 right-0 z-10 flex justify-between items-center'>
+      <div className='h-screen flex flex-col'>
+        <section className='flex justify-between items-center'>
           <div className='p-2'>
-            <h6 className='font-bold text-sm'>{project.project_name}</h6>
-            <h4 className='font-bold text-2xl'>{song.title}</h4>
+            <h4 className='font-bold text-2xl'>{project.project_name}</h4>
             <p>
-              {song.artist} | {formatTime(song.duration)}
+              {song.title} | {song.artist} | {formatTime(song.duration)}
             </p>
           </div>
-          <div className='p-2 w-[450px]'>
+          <div className='px-2 w-[450px]'>
             <AudioTimeline />
           </div>
         </section>
